c986565c4c56268188316c5e766963a99d903f01
CollidingAdversaries <tsaala@uni-bonn.de>
2025-05-16 10:39:11 +0200

    Add also preparing of submission for task 2

--- Task 1 - Adversarial Attack/Prepare_Submission.py
+++ Task 1 - Adversarial Attack/Prepare_Submission.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import zipfile
 import os
 
-# Load given test samples, assumes "Clean_Test_Public.feather" is in same folder
+# Load given test samples, assumes "Clean_Test_Public.feather" is in same directory
 clean_test_df = pd.read_feather('Clean_Test_Public.feather')
 
 # Separate clean test dataframe into x and y
@@ -13,7 +13,7 @@ y_clean = clean_test_df['Label'].to_numpy()
 
 # Insert here your logic to create adversaries, more parameters might be required
 def gen_advs(samples, model_path):
-    # Iff your adversaries rely on model information, load given pre-trained model, assumes "best_model.hdf5" is in same folder
+    # Iff your adversaries rely on model information, load given pre-trained model, assumes "best_model.hdf5" is in same directory
     model = tf.keras.models.load_model(model_path)
 
     # Do the actual adversarial generation here...
@@ -24,6 +24,19 @@ def gen_advs(samples, model_path):
 
 x_adv = gen_advs(x_clean, model_path='best_model.hdf5')
 
+# Optional: Test adversaries for Fooling Ratio and Mean Average Distance on the pre-trained model (assumes "best_model.hdf5" is in same directory):
+model = tf.keras.models.load_model('best_model.hdf5')
+adv_pred = (np.squeeze(model.predict(x_adv)) >= 0.5).astype(int)
+
+from sklearn.metrics import accuracy_score
+adv_fr = 1 - accuracy_score(y_clean, adv_pred)
+print(f"Fooling Ratio of adversarial samples: {adv_fr}")
+
+mean_avg_dist = np.mean(np.mean(np.abs(x_clean - x_adv), axis=1))
+print(f"Mean Average Distance: {mean_avg_dist}")
+
+score = adv_fr - (5 * mean_avg_dist)
+print(f"Final Score: {score}")
 
 # Create  list of triples (clean, adv, label)
 triples = [(x_clean[i], x_adv[i], y_clean[i]) for i in range(len(y_clean))]
